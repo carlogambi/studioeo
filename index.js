ee2eff885162fbd403650829a562060978fabd43
carlogambi <carlogambi@hotmail.it>
2020-03-06 16:45:58 +0100

    prova upload 7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const engines = require('consolidate');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
