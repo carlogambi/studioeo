242024a49b15451e5e409db5c41382cbc7efb3f1
carlogambi <carlogambi@hotmail.it>
2020-04-28 14:27:09 +0200

    17upld studioeov3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
@@ -5,13 +6,32 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(express.static(path.resolve(__dirname, 'libs')));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views', 'index.html'));
 });
 
-app.use(express.static(path.resolve(__dirname, 'dist')));
-app.use(express.static(path.resolve(__dirname, 'libs')));
-app.use(express.static(path.resolve(__dirname, 'public')));
+let exhibWinContent;
+app.post('/exhibPageMobile', (req, res) => {
+    exhibWinContent = req.body.arg; 
+    res.json({done: true});   
+});
+
+app.get('/exhibition', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views', 'exhibPageMobile.html'));
+});
+
+app.get('/exhibContent', (req, res) => {
+    res.json(exhibWinContent);   
+});
+
+
+
 
 http.listen(PORT, () => {
 console.log('ready to listen on port ' + PORT);
